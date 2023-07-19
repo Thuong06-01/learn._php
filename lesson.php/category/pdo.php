5b9046f854a302380e4d64d4a333101630df4f89
Thuong06-01 <133091089+Thuong06-01@users.noreply.github.com>
2023-07-19 22:02:44 +0700

    Update pdo.php

--- lesson.php/category/pdo.php
+++ lesson.php/category/pdo.php
@@ -1,6 +1,16 @@
 <?php
-    $conn = new PDO('mysql:host=localhost;dbname=test','root', '');
+$DB_TYPE = 'mysql';
+$DB_HOST = 'localhost';
+$DB_NAME = 'category';
+$DB_USER = 'root';
+$DB_PASS = '';
+
+try {
+    $conn = new PDO("{$DB_TYPE}:host={$DB_HOST};dbname={$DB_NAME}", $DB_USER, $DB_PASS);
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (Exception $e) {
+    die('Connect error: ' . $e->getMessage());
+}
 
     function prepareSQL($sql){
         global $conn;
